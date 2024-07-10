57f83b51877396713b24d90a8404794cc82d178f
swathisriprakash <swathisriprakash@gmail.com>
2024-07-10 15:17:15 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,21 @@
+var num=Math.floor(Math.random()*101);
+var count=0;
+console.log(num);
+
+function findnum()
+{
+var guessed_num=parseInt(document.getElementById("n1").value);
+count+=1;
+if(guessed_num==num)
+{
+    document.getElementById("result").textContent="congratulation!!!";
+}
+else if(guessed_num>num)
+{
+    document.getElementById ("result").textContent="Try with smaller number!!!";
+}
+else{
+    document.getElementById ("result").textContent="Try with greater number!!!";
+}
+document.getElementById("count").textContent=count;
+}
